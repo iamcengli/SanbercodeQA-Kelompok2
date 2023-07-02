6eee6e1dd282c0791e6b5104811b3385b1fb8490
iamcengli <harrygunawan932@gmail.com>
2023-07-02 22:34:21 +0700

    add test cases, add configuration

--- Pages/ContactMenuPage.py
+++ Pages/ContactMenuPage.py
@@ -12,14 +12,16 @@ class ContactMenuPage(BasePage):
     MESSAGE = (By.ID, "message-text")
     SEND_MESSAGE_BUTTON = (By.XPATH, "//button[contains(text(),'Send message')]") 
     CONTACT_MENU_LINK = (By.XPATH, "//a[contains(text(),'Contact')]")
+    X_BUTTON = (By.XPATH, "//div[@id='exampleModal']/div[@role='document']//div[@class='modal-header']/button[@type='button']/span[.='×']")
+    CLOSE_BUTTON = (By.XPATH, "//div[@id='exampleModal']/div[@role='document']//div[@class='modal-footer']/button[1]")
     
     def __init__(self, driver):
         super().__init__(driver)
         time.sleep(2)
         # self.driver.get(TestData.BASE_URL)
         
-    def is_alert_message_equal(self, alert_message):
-        return self.get_message(self.alert_message)
+    # def is_alert_message_equal(self, alert_message):
+    #     return self.get_message(self.alert_message)
         
     def do_open_contact_menu(self):
         self.do_click(self.CONTACT_MENU_LINK)
@@ -29,4 +31,10 @@ class ContactMenuPage(BasePage):
         self.do_send_keys(self.NAME, name)
         self.do_send_keys(self.MESSAGE, message)
         self.do_click(self.SEND_MESSAGE_BUTTON)
+        
+    def do_close_form_close_button(self):
+        self.do_click(self.CLOSE_BUTTON)
+ 
+    def do_close_form_x_button(self):
+        self.do_click(self.X_BUTTON)
         
--- Tests/test_ContactMenuPage.py
+++ Tests/test_ContactMenuPage.py
@@ -7,8 +7,25 @@ from selenium.webdriver.common.alert import Alert
 
 
 class Test_Contact_Menu(BaseTest):
-        
+    
+    # TC6-001
     def test_send_message(self):
+        self.contactMenu = ContactMenuPage(self.driver)
+        self.contactMenu.do_open_contact_menu()
+        self.contactMenu.do_send_message(TestData.CM_EMAIL_VALID, TestData.CM_NAME, TestData.CM_MESSAGE)
+        time.sleep(1.5)
+        #validasi alert
+        alert = Alert(self.driver)
+        actual_alert_message=alert.text
+        expected_alert_message = TestData.CM_RESPONSE_MESSAGE #pesan yang diharapkan
+        self.assertEqual(expected_alert_message, actual_alert_message)
+        assert actual_alert_message == expected_alert_message
+        alert.accept() #click alert
+        # self.contactMenu.is_alert_message_equal(TestData.CM_MESSAGE)
+        self.contactMenu.close()
+        
+    # TC6-002 
+    def test_failed_send_message1(self):
         self.contactMenu = ContactMenuPage(self.driver)
         self.contactMenu.do_open_contact_menu()
         self.contactMenu.do_send_message(TestData.CM_EMAIL_INVALID, TestData.CM_NAME, TestData.CM_MESSAGE)
@@ -16,13 +33,53 @@ class Test_Contact_Menu(BaseTest):
         #validasi alert
         alert = Alert(self.driver)
         actual_alert_message=alert.text
-        expected_alert_message = "Thanks for the message!!" #pesan yang diharapkan
+        expected_alert_message = TestData.CM_RESPONSE_INVALID_EMAIL #pesan yang diharapkan
         self.assertEqual(expected_alert_message, actual_alert_message)
         assert actual_alert_message == expected_alert_message
         alert.accept() #click alert
         # self.contactMenu.is_alert_message_equal(TestData.CM_MESSAGE)
         self.contactMenu.close()
         
-    # def test_check_message(self):
-    #     contact_page = ContactMenuPage(self.driver)
+    # TC6-003 
+    def test_failed_send_message2(self):
+        self.contactMenu = ContactMenuPage(self.driver)
+        self.contactMenu.do_open_contact_menu()
+        self.contactMenu.do_send_message(TestData.CM_EMAIL_INVALID, TestData.EMPTY, TestData.CM_MESSAGE)
+        time.sleep(1.5)
+        #validasi alert
+        alert = Alert(self.driver)
+        actual_alert_message=alert.text
+        expected_alert_message = TestData.CM_RESPONSE_FAILED_MESSAGE #pesan yang diharapkan
+        self.assertEqual(expected_alert_message, actual_alert_message)
+        assert actual_alert_message == expected_alert_message
+        alert.accept() #click alert
+        # self.contactMenu.is_alert_message_equal(TestData.CM_MESSAGE)
+        self.contactMenu.close()
         
+    # TC6-004 
+    def test_failed_send_message3(self):
+        self.contactMenu = ContactMenuPage(self.driver)
+        self.contactMenu.do_open_contact_menu()
+        self.contactMenu.do_send_message(TestData.CM_EMAIL_INVALID, TestData.CM_NAME, TestData.EMPTY)
+        time.sleep(1.5)
+        #validasi alert
+        alert = Alert(self.driver)
+        actual_alert_message=alert.text
+        expected_alert_message = TestData.CM_RESPONSE_FAILED_MESSAGE #pesan yang diharapkan
+        self.assertEqual(expected_alert_message, actual_alert_message)
+        assert actual_alert_message == expected_alert_message
+        alert.accept() #click alert
+        # self.contactMenu.is_alert_message_equal(TestData.CM_MESSAGE)
+        self.contactMenu.close()
+       
+    # TC6-005 
+    def test_cancel_send_message1(self):
+        self.contactMenu = ContactMenuPage(self.driver)
+        self.contactMenu.do_open_contact_menu()
+        self.contactMenu.do_close_form_close_button()        
+    
+    # TC6-006
+    def test_cancel_send_message2(self):
+        self.contactMenu = ContactMenuPage(self.driver)
+        self.contactMenu.do_open_contact_menu()
+        self.contactMenu.do_close_form_x_button()        
